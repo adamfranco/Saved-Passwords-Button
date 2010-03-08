023390e93c915b31bc2573726fe57984559a75d3
Adam Franco <afranco@middlebury.edu>
2010-03-07 21:14:44 -0500

    Basic template created with the Firefox/Thunderbird Extension Wizard.
    http://ted.mielczarek.org/code/mozilla/extensionwiz/

---
+++ defaults/preferences/savedpasswords.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.savedpasswords@adamfranco.com.description", "chrome://savedpasswords/locale/savedpasswords.properties");
