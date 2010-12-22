be111d04f0b21fb03a314fe2795dec7b8120fc26
Adam Franco <afranco@middlebury.edu>
2010-12-21 23:21:11 -0500

    The button is now added to the toolbar by default when installing.
    Tested in FF 3.6.3 and 4.0B8.
    
    This fixes part(4) of Issue #1:
    	"4) add/ remove button to Navigation toolbar (value ´add´ by default)"
    https://github.com/adamfranco/Saved-Passwords-Button/issues#issue/1

--- defaults/preferences/savedpasswords.js
+++ defaults/preferences/savedpasswords.js
@@ -1,2 +1,3 @@
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.savedpasswords@adamfranco.com.description", "chrome://savedpasswords/locale/savedpasswords.properties");
+pref("extensions.savedpasswords@adamfranco.com.buttonInstalled", false);
